3f566b4dd0b43e7a60e5afb10ace3327e58dd4d9
warlordlizard <alexjbowers88@gmail.com>
2018-03-12 09:08:04 -0700

    editing models

--- model/company.js
+++ model/company.js
@@ -12,11 +12,11 @@ const companySchema = Schema({
   zip: { type: String },
   phone: { type: String },
   companyNotes: { type: String },
-  contacts: { type: Array },
-  jobPosting: { type: Array },
-  events: { type: Array },
-  userId: { type: Schema. Types.ObjectId, required: true },
-  profileId: { type: Schema. Types.ObjectId, required: true },
+  contacts: { type: Array, default: [] },
+  jobPosting: { type: Array, default: [] },
+  events: { type: Array, default: [] },
+  userId: { type: Schema.Types.ObjectId, required: true },
+  profileId: { type: Schema.Types.ObjectId, required: true },
   created: { type: Date, default: Date.now },
 });
 
