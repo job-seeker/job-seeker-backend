a0900df130b8f9722baa088046dff7b832e36458
warlordlizard <alexjbowers88@gmail.com>
2018-03-14 15:00:16 -0700

    refactored user test a bit

--- __test__/user-router.test.js
+++ __test__/user-router.test.js
@@ -16,84 +16,75 @@ describe('User Auth Routes', function() {
   afterAll( done => serverToggle.serverOff(server, done));
 
   describe('POST: /api/signup', function() {
-    describe('with a valid body', function() {
-      afterEach ( done => {
-        User.remove({})
-          .then( () => done())
-          .catch(done);
-      });
-      it('should return a token', done => {
-        request.post(`${url}/api/signup`)
-          .send(exampleUser)
-          .end((err, res) => {
-            if (err) return done(err);
-            expect(res.status).toEqual(200);
-            expect(typeof res.text).toEqual('string');
-            done();
-          });
-      });
+    afterEach ( done => {
+      User.remove({})
+        .then( () => done())
+        .catch(done);
     });
-    describe('without a valid body', function() {
-      it('should return a 400 error', done => {
-        request.post(`${url}/api/signup`)
-          .send({})
-          .end((err, res) => {
-            expect(res.status).toEqual(400);
-            expect(res.text).toEqual('BadRequestError');
-            done();
-          });
-      });
+    it('should return a token with a valid body', done => {
+      request.post(`${url}/api/signup`)
+        .send(exampleUser)
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.status).toEqual(200);
+          expect(typeof res.text).toEqual('string');
+          done();
+        });
+    });
+    it('should return a 400 error without a valid body', done => {
+      request.post(`${url}/api/signup`)
+        .send({})
+        .end((err, res) => {
+          expect(res.status).toEqual(400);
+          expect(res.text).toEqual('BadRequestError');
+          done();
+        });
     });
   });
 
   describe('GET: /api/signin', function() {
-    describe('with a valid body', function() {
-      beforeEach( done => {
-        let user = new User(exampleUser);
-        user.generatePasswordHash(exampleUser.password)
-          .then( user => user.save())
-          .then( user => {
-            this.tempUser = user;
-            done();
-          })
-          .catch(done);
-      });
-      afterEach( done => {
-        User.remove({})
-          .then( () => done())
-          .catch(done);
-      });
-      it('should return a token', done => {
-        request.get(`${url}/api/signin`)
-          .auth(exampleUser.username, exampleUser.password)
-          .end((err, res) => {
-            if (err) return done(err);
-            expect(res.status).toEqual(200);
-            expect(typeof res.text).toEqual('string');
-            done();
-          });
-      });
+    beforeEach( done => {
+      let user = new User(exampleUser);
+      user.generatePasswordHash(exampleUser.password)
+        .then( user => user.save())
+        .then( user => {
+          this.tempUser = user;
+          done();
+        })
+        .catch(done);
     });
-    describe('without a valid body', function() {
-      it('should return a 401', done => {
-        request.get(`${url}/api/signin`)
-          .end((err, res) => {
-            expect(res.status).toEqual(401);
-            expect(res.text).toEqual('UnauthorizedError');
-            done();
-          });
-      });
+    afterEach( done => {
+      User.remove({})
+        .then( () => done())
+        .catch(done);
     });
-    describe('with an invalid username and password', function() {
-      it('should return a 500 error', done => {
-        request.get(`${url}/api/signin`)
-          .auth('fakeusername', 'fakepassword')
-          .end((err, res) => {
-            expect(res.status).toEqual(500);
-            expect(res.text).toEqual('InternalServerError');
-            done();
-          });
-      });
+    it('should return a token with a valid body', done => {
+      request.get(`${url}/api/signin`)
+        .auth(exampleUser.username, exampleUser.password)
+        .end( (err, res) => {
+          if (err) return done(err);
+          expect(res.status).toEqual(200);
+          expect(typeof res.text).toEqual('string');
+          done();
+        });
     });
+    it('should return a 401 error without a valid body', done => {
+      request.get(`${url}/api/signin`)
+        .end( (err, res) => {
+          expect(res.status).toEqual(401);
+          expect(res.text).toEqual('UnauthorizedError');
+          done();
+        });
+    });
+    it('should return a 500 error with an invalid username and password', done => {
+      request.get(`${url}/api/signin`)
+        .auth('fakeusername', 'fakepassword')
+        .end( (err, res) => {
+          expect(res.status).toEqual(500);
+          expect(res.text).toEqual('InternalServerError');
+          done();
+        });
+    });
+    
   });
 });
