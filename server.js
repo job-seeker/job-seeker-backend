a8bf6ac2c10b3073918dad2c627027567c5fda0a
warlordlizard <alexjbowers88@gmail.com>
2018-03-15 12:07:11 -0700

    removed tries on user.js, serverOn and validation error from middleware

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const errors = require('./lib/error-middleware.js');
 dotenv.load();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGODB_URI);
 
 app.use(cors());
