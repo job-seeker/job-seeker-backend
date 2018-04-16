519158a6d9a3984a4cbbbf6ca02c3a00837e9935
warlordlizard <alexjbowers88@gmail.com>
2018-04-16 15:12:01 -0700

    auth0 route and took username off user schema

--- route/user-router.js
+++ route/user-router.js
@@ -3,33 +3,16 @@
 const jsonParser = require('body-parser').json();
 const debug = require('debug')('job-seeker: user-router');
 const Router = require('express').Router;
-const basicAuth = require('../lib/basic-auth-middleware.js');
 const User = require('../model/user.js');
-const createError = require('http-errors');
 
 const userRouter = module.exports = Router();
 
-userRouter.post('/api/signup', jsonParser, function(req, res, next) {
-  debug('POST: /api/signup');
-  if(!req.body.username) return next(createError(400, 'ValidationError'));
+userRouter.post('/api/handleAuth', jsonParser, function (req, res, next) {
+  debug('GET: /api/handleAuth');
 
-  let password = req.body.password;
-  delete req.body.password;
-
-  let user = new User(req.body);
-  user.generatePasswordHash(password)
-    .then( user => user.save())
-    .then( user => user.generateToken())
-    .then( token => res.send(token))
-    .catch(next);
-});
-
-userRouter.get('/api/signin', basicAuth, function(req, res, next) {
-  debug('GET: /api/signin');
-
-  User.findOne({ username: req.auth.username })
-    .then( user => user.comparePasswordHash(req.auth.password))
-    .then( user => user.generateToken())
-    .then( token => res.send(token))
+  User.findOne({ email: req.body.email })
+    .then(user => user ? user : new User(req.body).save())
+    .then(user => user.generateToken())
+    .then(token => res.send(token))
     .catch(next);
 });
