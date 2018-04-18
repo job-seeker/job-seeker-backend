18fa8ea688f02ad749725f9be40ac1534a352e77
Melanie Cohen <melaniebcohen@gmail.com>
2018-04-18 15:54:08 -0700

    route updates for frontend

--- model/company.js
+++ model/company.js
@@ -30,6 +30,9 @@ const Company = module.exports = mongoose.model('company', companySchema);
 Company.findByIdAndAddJob = function(id, job) {
   debug('findByIdAndAddJob');
   
+  console.log(id)
+  console.log(job)
+  
   return Company.findById(id)
     .then( company => {
       job.userId = company.userId;
