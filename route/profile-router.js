b24b9bccf9a19ea1f15b82ceafee5096c70f7980
Melanie Cohen <melaniebcohen@gmail.com>
2018-04-18 22:12:55 -0700

    router updates from frontend work

--- route/profile-router.js
+++ route/profile-router.js
@@ -24,7 +24,14 @@ profileRouter.get('/api/profile/', bearerAuth, function(req, res, next) {
   debug('GET: /api/profile/');
 
   Profile.findOne({ userId: req.user._id })
-    .populate('companies')
+    .populate({
+      path: 'companies',
+      populate: [
+        { path: 'jobPosting' },
+        { path: 'events' },
+        { path: 'contacts' },
+      ],
+    })
     .then( profile => res.json(profile))
     .catch(next);
 });
